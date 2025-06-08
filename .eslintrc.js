5c7b41f712256053bb321f0049348893a2472e00
Jan Prokop <jan.prokop@etnetera.eu>
2025-06-08 15:34:35 +0200

    Configs changes.

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,17 @@ module.exports = {
   ],
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        args: 'all',
+        argsIgnorePattern: '^_',
+        caughtErrors: 'all',
+        caughtErrorsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+      },
+    ],
   },
 };
